3ade67d987cef8cf69184e63077b6494eb8b4eac
ifabrisarabellapark <fabris.irene95@gmail.com>
2022-11-05 21:10:25 -0700

    core logic to store scores

--- polygon/README.md
+++ polygon/README.md
@@ -1,4 +1,5 @@
 ### Solidity env set up
 ```bash
 npm install --save-dev "hardhat@^2.12.0"
+npm install @openzeppelin/contracts
 ```
--- polygon/contracts/StoreScores.sol
+++ polygon/contracts/StoreScores.sol
@@ -1,7 +1,35 @@
 pragma solidity ^0.8.17;
+pragma experimental ABIEncoderV2;
 // SPDX-License-Identifier: GPL-3.0
 
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+
+
 //contract
-contract StoreScores {
-    // skeleton
+contract StoreScores is Ownable {
+    struct User {
+        int16 score;
+        string description;
+    }
+
+    //define members for your smart contract
+    mapping(address => User[]) public records;
+    uint256 public userCount;
+    uint256 public scoreCount;
+
+    constructor() {
+        userCount = 0;
+        scoreCount = 0;
+    }
+
+    function uploadScore(
+        int16 _score,
+        string calldata _description,
+        address _beneficiary,
+        uint256 _amount
+    ) public payable returns (bool b) {
+        b = false;
+        // ...
+        return b;
+    }
 }
